ada3df195b4aae668e63d1ece5f90170fedbd3d9
agryva <irvan.lutfi.gunawan@gmail.com>
2017-03-31 18:59:22 +0700

    Add Lib (Alpha)

--- lib/lib.php
+++ lib/lib.php
@@ -5,11 +5,46 @@
 	*/
 	class Database 
 	{
+		public var $mysqli = "";
+		public var $tblname = "";
+		public var $fieldname = '';
+		public var $value = '';
 		
 		function __construct()
 		{
-			# code...
+			$this->mysqli = new mysqli('localhost','root','','');
 		}
+
+		function getAll($tbname){
+			if(!empty($tbname))
+				$data = $this->mysqli->query("SELECT * FROM $tbname");
+			$res = array();
+			while ($datas = $data->fetch_object()) {
+				$res = $datas;
+			}
+			return $res;
+		}
+
+		function get($tbname){
+			$this->tblname = $tbname;
+		}
+
+		function where($fieldname,$value){
+			$this->fieldname = $fieldname;
+			$this->value = $value;
+		}
+
+		function getwhere($fieldname,$value){
+			$data = $this->mysqli->query("SELECT * FROM $this->tblname WHERE $fieldname = '$value'");
+			$res = array();
+			while ($datas = $data->fetch_object()) {
+				$res[] = $datas;
+			}
+			return $res;
+		}
+
 	}
+
+	$db = new Database();
 
  ?>
