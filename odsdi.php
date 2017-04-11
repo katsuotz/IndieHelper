171c607b3788acc76766ca71d24dc1a6287b7236
Leonona <jalu717@gmail.com>
2017-04-11 09:17:28 +0700

    Add io

---
+++ odsdi.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title></title>
+    <link type="text/css" rel="stylesheet" href="lib/materialize/css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="lib/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="lib/css/main.css">
+    </head>
+        <body>
+      
+   <div class="row">
+       <div class="col s12 red"><p>s12</p></div></div>
+       <div class="row">
+    <div class="col s12 m4 l2 red"><p>s12 m4</p></div>
+    <div class="col s12 m4 l8 red"><p>s12 m4</p></div>
+    <div class="col s12 m4 l2 red"><p>s12 m4</p></div>
+  </div>
+  <div class="row">
+    <div class="col s12 m6 l3 red"><p>s12 m6 l3</p></div>
+    <div class="col s12 m6 l3 red"><p>s12 m6 l3</p></div>
+    <div class="col s12 m6 l3 red"><p>s12 m6 l3</p></div>
+    <div class="col s12 m6 l3 red"><p>s12 m6 l3</p></div>
+  </div>
+
+
+
+            <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+            <script type="text/javascript" src="lib/materialize/js/materialize.min.js"></script>
+        </body>
+</html>
