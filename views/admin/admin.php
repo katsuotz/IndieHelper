181510dbe3da56ddf86e2087fc07b184d922eb9e
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2017-04-12 18:13:05 +0700

    rapii

--- views/admin/admin.php
+++ views/admin/admin.php
@@ -1,31 +1,4 @@
 <div class="content">
-	
-	<?php 
-
-		$value = array(
-			'nama' => 'gg',
-			'kelas' => 'wp',
-			'username' => 'ez'
-			);
-
-		$row = array();
-		$col = array();
-
-		foreach ($value as $key => $values) {
-			array_push($row, $key);
-			array_push($col, $values);
-		}
-
-		$result = array();
-
-		for ($i = 0; $i < count($row); $i++) {
-			array_push($result, ($row[$i] . " = '" . $col[$i]) . "'");
-		}
-
-		print_r($result);
-		echo '<br>WHERE ' . implode(" AND ", $result);
-
-	?>
 	<div id="dashboard" class="col s12">Test 1</div>
 	<div id="user" class="col s12">Test 2</div>
 	<div id="project" class="col s12">Test 3</div>
