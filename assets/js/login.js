c1768c76a3e0d6a9fbe1b11dd901a867c9701c32
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2017-04-14 05:50:12 +0700

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,6 +13,19 @@ $(document).ready(function() {
 			$('#form-sign-up')[0].reset();
 			Materialize.toast('Sign Up ' + result, 4000);
 		})
+	});
+
+	$('#form-log-in').submit(function(e) {
+		console.log(1);
+		e.preventDefault();
+		$.ajax({
+			url: $(this).attr('action'),
+			type: $(this).attr('method'),
+			data: $(this).serialize(),
+		})
+		.done(function(result) {
+			Materialize.toast('Log In ' + result, 4000);
+		})
 		
 	});
 });
