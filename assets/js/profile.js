3a5b7cef98dd71b9221e16f62da909965ccf4f17
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2017-04-13 18:48:57 +0700

    profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 	$('.parallax').parallax();
+	x = 1;
+	$('.follow-btn').click(function() {
+		if (x == 1) {
+			$(this).removeClass('white black-text');
+			$(this).addClass('purple white-text');
+			$(this).html('Following');
+			x = 0;
+		} else {
+			$(this).addClass('white black-text');
+			$(this).removeClass('purple white-text');
+			$(this).html('Follow');
+			x = 1;
+		}
+	});
+	
 });
--- views/user/profile.php
+++ views/user/profile.php
@@ -5,39 +5,153 @@
 <div class="content container">
 	<div class="row pt3">
 		<div class="col s12 mt3">
-			<div class="card white mt3">
-				<div class="card-content pink lighten-1 white-text col s12">
-					<div class="center-align col s5">
+			<div class="card profile-card white mt3">
+				<div class="card-content purple lighten-1 white-text col s12">
+					<div class="center-align col s4">
 						<span class="card-image-profile"><img src="<?= baseurl('assets/images/user/default.jpg') ?>" class="circle my1"></span>
 					</div>
-					<div class="left-align col s7 pt2">
+					<div class="left-align col s8 pt2">
 						<div class="row m0 py0 pl0 pr1">
-							<span class="card-title left">Azunyan </span> <a class="waves-effect waves-shadow btn right grey lighten-3 black-text">Edit Profile</a>
+							<span class="card-title left m0">Azunyan </span>
+							<a class="waves-effect waves-shadow btn right grey lighten-3 black-text">Edit Profile</a>
+						</div>
+						<div class="row m0 pb1">
+							<span>@azunyan</span>
 						</div>
 						<div class="row m0 p0">
 							<p class="pb1">
 								<b>13</b> Project &nbsp;
 								<b>57</b> Followers &nbsp;
+								<b>34</b> Following &nbsp;
 							</p>
-							<p>@azunyan</p>
-							<p>Just shut up.</p>
+						</div>
+						<div class="row m0 p0">
+							<button class="btn white black-text follow-btn waves-effect waves-shadow">Follow</button>
 						</div>
 					</div>
 				</div>
-				<div>
-					<div class="card-tabs">
-						<ul class="tabs">
-							<li class="tab col s3"><a class="active" href="#test1">Dashboard</a></li>
-							<li class="tab col s3"><a class="active" href="#test2">Project</a></li>
-							<li class="tab col s3"><a href="#test3">Setting</a></li>
-							<li class="tab col s3"><a href="#test4">Points</a></li>
-						</ul>
+				<div class="card-tabs">
+					<ul class="tabs tabs-fixed-width">
+						<li class="tab col s3"><a class="purple-text active" href="#test1">Profile</a></li>
+						<li class="tab col s3"><a class="purple-text" href="#test2">Activity</a></li>
+						<li class="tab col s3"><a class="purple-text" href="#test3">Contributions</a></li>
+						<li class="tab col s3"><a class="purple-text" href="#test4">Project</a></li>
+					</ul>
+				</div>
+				<div class="card-content py2">
+					<div id="test1" class="container">
+						<div class="row pb1 m0">
+							<div class="col s4"><b>Age</b></div>
+							<div class="col s8">21</div>
+						</div>
+						<div class="row pb1 m0">
+							<div class="col s4"><b>Address</b></div>
+							<div class="col s8">Jalan Kliningan No. 6 Kota Bandung</div>
+						</div>
+						<div class="row pb1 m0">
+							<div class="col s4"><b>Email</b></div>
+							<div class="col s8">azu@nyan.com</div>
+						</div>
+						<div class="row pb1 m0">
+							<div class="col s4"><b>About</b></div>
+							<div class="col s8"><p class="justify-align">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p></div>
+						</div>
 					</div>
-					<div class="card-content">
-						<div id="test1" class="col s12">Test 1</div>
-						<div id="test2" class="col s12">Test 2</div>
-						<div id="test3" class="col s12">Test 3</div>
-						<div id="test4" class="col s12">Test 4</div>
+					<div id="test2" class="col s12">Test 2</div>
+					<div id="test3" class="col s12">Test 3</div>
+					<div id="test4" class="container-fluid my-project">
+						<div class="row">
+							<div class="col s12 m4">
+								<div class="card">
+									<div class="card-image">
+										<img src="<?= baseurl('assets/images/user/default.jpg') ?>">
+										<span class="card-title">Card Title</span>
+									</div>
+									<div class="card-content">
+										<p>I am a very simple card. I am good at containing small bits of information.
+										I am convenient because I require little markup to use effectively.</p>
+									</div>
+									<div class="card-action right-align">
+										<a href="#" class="purple-text mr1 see-details">See Details</a>
+									</div>
+								</div>
+							</div>
+							<div class="col s12 m4">
+								<div class="card">
+									<div class="card-image">
+										<img src="<?= baseurl('assets/images/login/banner.jpg') ?>">
+										<span class="card-title">Card Title</span>
+									</div>
+									<div class="card-content">
+										<p>I am a very simple card. I am good at containing small bits of information.
+										I am convenient because I require little markup to use effectively.</p>
+									</div>
+									<div class="card-action right-align">
+										<a href="#" class="purple-text mr1 see-details">See Details</a>
+									</div>
+								</div>
+							</div>
+							<div class="col s12 m4">
+								<div class="card">
+									<div class="card-image">
+										<img src="<?= baseurl('assets/images/login/banner.jpg') ?>">
+										<span class="card-title">Card Title</span>
+									</div>
+									<div class="card-content">
+										<p>I am a very simple card. I am good at containing small bits of information.
+										I am convenient because I require little markup to use effectively.</p>
+									</div>
+									<div class="card-action right-align">
+										<a href="#" class="purple-text mr1 see-details">See Details</a>
+									</div>
+								</div>
+							</div>
+							<div class="col s12 m4">
+								<div class="card">
+									<div class="card-image">
+										<img src="<?= baseurl('assets/images/user/default.jpg') ?>">
+										<span class="card-title">Card Title</span>
+									</div>
+									<div class="card-content">
+										<p>I am a very simple card. I am good at containing small bits of information.
+										I am convenient because I require little markup to use effectively.</p>
+									</div>
+									<div class="card-action right-align">
+										<a href="#" class="purple-text mr1 see-details">See Details</a>
+									</div>
+								</div>
+							</div>
+							<div class="col s12 m4">
+								<div class="card">
+									<div class="card-image">
+										<img src="<?= baseurl('assets/images/user/default.jpg') ?>">
+										<span class="card-title">Card Title</span>
+									</div>
+									<div class="card-content">
+										<p>I am a very simple card. I am good at containing small bits of information.
+										I am convenient because I require little markup to use effectively.</p>
+									</div>
+									<div class="card-action right-align">
+										<a href="#" class="purple-text mr1 see-details">See Details</a>
+									</div>
+								</div>
+							</div>
+							<div class="col s12 m4">
+								<div class="card">
+									<div class="card-image">
+										<img src="<?= baseurl('assets/images/login/banner.jpg') ?>">
+										<span class="card-title">Card Title</span>
+									</div>
+									<div class="card-content">
+										<p>I am a very simple card. I am good at containing small bits of information.
+										I am convenient because I require little markup to use effectively.</p>
+									</div>
+									<div class="card-action right-align">
+										<a href="#" class="purple-text mr1 see-details">See Details</a>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
