d15129c95cb5ecf4d3f91ae6f0d0f247a0745784
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2017-04-18 13:51:33 +0700

    navadmin

--- views/template/navbar_admin.php
+++ views/template/navbar_admin.php
@@ -76,3 +76,28 @@
 	</ul>
 	
 	<div class="all">
+		<!-- Dropdown Structure -->
+		<ul id="dropdown1" class="dropdown-content">
+			<li><a href="#!">one</a></li>
+			<li><a href="#!">two</a></li>
+			<li class="divider"></li>
+			<li><a href="#!">three</a></li>
+		</ul>
+		<nav>
+			<div class="nav-wrapper">
+				<a href="#!" class="brand-logo white-text">Logo</a>
+				<!-- activate side-bav in mobile view -->
+				<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+				<ul class="right hide-on-med-and-down">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="components.html">Components</a></li>
+					<!-- Dropdown Trigger -->
+					<li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+				</ul>
+				<!-- navbar for mobile -->
+				<ul class="side-nav" id="mobile-demo">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="components.html">Components</a></li>
+				</ul>
+			</div>
+		</nav>
