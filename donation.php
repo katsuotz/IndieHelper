c87cd45608992e4231b20a2d43d1fb9967d69957
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2017-04-26 13:26:11 +0700

    donation

--- donation.php
+++ donation.php
@@ -1,14 +1,17 @@
 <?php 
 	
-	include 'lib/lib.php';
+include 'lib/lib.php';
 
-	$assets = array(
-		'css' 	=> array('parent.css', 'donasi.css', 'materialize.min.css', 'material-icon.css', 'font-awesome.min.css'),
-		'js'	=> array('jquery.min.js', 'materialize.min.js', 'parent.js','login.js'),
-		);
+$assets = array(
+	'css' 	=> array('parent.css', 'donasi.css', 'materialize.min.css', 'material-icon.css', 'font-awesome.min.css'),
+	'js'	=> array('jquery.min.js', 'materialize.min.js', 'parent.js','donation.js'),
+	);
 
-	include 'views/template/header.php';
-	include 'views/user/donation.php';
-	include 'views/template/footer_admin.php';
+$db->get_tbl('bank');
+$bank = $db->result();
 
- ?>
+include 'views/template/header.php';
+include 'views/user/donation.php';
+include 'views/template/footer_admin.php';
+
+?>
