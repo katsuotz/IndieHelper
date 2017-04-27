087f5c55b03b7dcb9b77dbe649bc1e08db5012ff
agryva <irvan.lutfi.gunawan@gmail.com>
2017-04-27 10:42:56 +0700

    Add Action EditProject

--- assets/js/edit-project.js
+++ assets/js/edit-project.js
@@ -13,6 +13,39 @@ $(document).ready(function($) {
     	data: $chipstags
 	});
 
+  	$('.choose-file').bind('click', function(e) {
+		idFile = $(this).attr('id');
+		idFile = idFile.charAt(11);
+		var defaultImage = base_url + 'assets/images/project/default-project.jpg';
+		if ($('#uploaded-project-image' + idFile).attr('src') == defaultImage) {
+			console.log(1);
+			$('#project-image' + idFile).click();
+		} else {
+			console.log(2);
+			$('#uploaded-project-image' + idFile).attr('src', defaultImage);
+			$('#project-image' + idFile).val('');
+			$('#choose-file' + idFile + ' .fa').addClass('fa-plus').removeClass('fa-remove');
+		}
+	});
+
+	$('.project-image').change(function(e) {
+		var idInputFile = $(this).attr('name');
+		readImagePath(this, idInputFile.charAt(13));
+	});
+
+	function readImagePath(input, target) {
+		if (input.files[0]) {
+			$('#choose-file' + target + ' .fa').removeClass('fa-plus').addClass('fa-remove');
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				$('#uploaded-project-image' + target).attr('src', e.target.result).removeClass('hidden');
+			};
+			reader.readAsDataURL(input.files[0]);
+		} else {
+			$('#uploaded-project-image' + target).attr('src', '#');
+		}
+	}
+
 	$('#update-project').submit(function(e) {
 		e.preventDefault();
 
--- lib/EditProject.php
+++ lib/EditProject.php
@@ -4,27 +4,58 @@
 
 	if(($_SERVER['REQUEST_METHOD'] == 'POST')){
 		$id 		= $input->get('id_project');
+		$where 		= array('id_project' => $id);
 		$nama		= $input->post('nama');
 		$desc		= $input->post('desc');
 		$donation	= $input->post('donation');
 		$kategori 	= $input->post('id_kategori');
 		$j_tags 	= json_decode($input->post('tags'));
 		$tags = array();
-
+		$db->get_tbl('project');
+		$result 	= $db->row_result();
+		$oldImages 	= json_decode($result->foto);
+ 
 		foreach ($j_tags as $key => $value) {
 			array_push($tags, $value);
 		}
 		$taga = implode($tags, ',');
 
+
+		$namafile 	= array();
+		$tmpfile  	= array();
+		for ($i = 1; $i <= 5; $i++) {
+			if (!(empty($input->files('project-image' . $i)['name']))) {
+				array_push($namafile, $input->files('project-image' . $i)['name']);
+				array_push($tmpfile, $input->files('project-image' . $i)['dir']);
+				$dir = "../assets/images/project/" . $namafile[$i - 1]; 
+				move_uploaded_file($tmpfile[$i - 1], $dir);
+			} else {
+				array_push($namafile, '');
+				array_push($tmpfile, '');
+			}
+		}
+
+		for ($i = 1; $i <= 5; $i++) {
+			if (empty($namafile[$i - 1]) && !(empty($oldImages[$i - 1]))) {
+				$namafile[$i - 1] = $oldImages[$i - 1];
+			}
+		}
+
+		if (!empty($namafile)) {
+			$namafile = json_encode($namafile);
+		} else {
+			$namafile = []; 
+		}
+
+
 		$data = array(
 			'nama' 		=> $nama,
 			'deskripsi' => $desc,
 			'target'	=> $donation,
+			'foto'		=> $namafile,
 			'tags'		=> $taga,
 			);
-		$where = array(
-			'id_project' => $id,
-			);
+
 		$db->where($where);
 		$db->select_tbl('project');
 		$return = $db->update($data);
