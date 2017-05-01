3033488edbfddd23c54b93dc362554d4dfc1d94a
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2017-05-01 18:06:13 +0700

    login

--- lib/LogIn.php
+++ lib/LogIn.php
@@ -23,14 +23,18 @@
 				$db->get_tbl('akun');
 				$result_admin = $db->row_result();
 
-				$data = array(
-					'ID'		=> $result_admin->id_admin,
-					'ID_AKUN' 	=> $result_admin->id_akun,
-					'USERNAME' 	=> $result_admin->username,
-					'STATUS'	=> $result_admin->status,
-					'NAMA'		=> $result_admin->nama,
-					'FOTO'		=> $result_admin->foto
-					);
+				if ($result_admin) {
+					$data = array(
+						'ID'		=> $result_admin->id_admin,
+						'ID_AKUN' 	=> $result_admin->id_akun,
+						'USERNAME' 	=> $result_admin->username,
+						'STATUS'	=> $result_admin->status,
+						'NAMA'		=> $result_admin->nama,
+						'FOTO'		=> $result_admin->foto
+						);
+					$session->set_session($data);
+				}
+
 			} elseif ($result->status == 'user') {
 				$where = array('akun.id_akun' => $result->id_akun);
 				$db->join('user', '', 'akun.id_akun', 'user.id_akun');
@@ -38,20 +42,21 @@
 				$db->get_tbl('akun');
 				$result_user = $db->row_result();
 				
-				$data = array(
-					'ID'		=> $result_user->id_user,
-					'ID_AKUN' 	=> $result_user->id_akun,
-					'USERNAME' 	=> $result_user->username,
-					'STATUS'	=> $result_user->status,
-					'NAMA'		=> $result_user->nama,
-					'FOTO'		=> $result_user->foto
-					);
+				if ($result_user) {
+					$data = array(
+						'ID'		=> $result_user->id_user,
+						'ID_AKUN' 	=> $result_user->id_akun,
+						'USERNAME' 	=> $result_user->username,
+						'STATUS'	=> $result_user->status,
+						'NAMA'		=> $result_user->nama,
+						'FOTO'		=> $result_user->foto
+						);
+					$session->set_session($data);
+				}
 			}
-			$session->set_session($data);
-			echo $session->get_session('STATUS');
-		} else {
-			echo $session->get_session('STATUS');
 		}
+
+		echo $session->get_session('STATUS');
 
 	}
 
