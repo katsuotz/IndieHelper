9200d49c325d1fb352c59cd9a5c7fa7e7c3d0f79
agryva <irvan.lutfi.gunawan@gmail.com>
2017-05-08 18:45:01 +0700

    Explore

--- explore.php
+++ explore.php
@@ -1,19 +1,31 @@
 <?php 
 	include 'lib/lib.php';
-error_reporting(0);
+	// error_reporting(0);
+	$foto =array();
+
 	$assets = array(
 		'css' 	=> array('parent.css', 'explore.css', 'materialize.min.css', 'material-icon.css', 'font-awesome.min.css'),
 		'js'	=> array('jquery.min.js', 'materialize.min.js', 'parent.js', 'explore.js')
 	);
-	$db->get_tbl('project');
-	$data = $db->result();
-	$foto =array();
+
+	if(isset($_GET['ktg'])){
+		$ktg = $input->get('ktg');
+		$where = array('id_kategori' => $ktg);
+		$db->where($where);
+		$db->get_tbl('project');
+		$data = $db->result();
+	}
+	else{
+		$db->get_tbl('project');
+		$data = $db->result();
+	}
 	foreach ($data as $key => $value) {
 		array_push($foto, json_decode($value->foto));
-
 	}
 
+
 	include 'views/template/header.php';
+	include 'views/template/nanavbaran.php';
 	include 'views/explore/explore.php';
 	include 'views/template/footer_admin.php';
 ?>
